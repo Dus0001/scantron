8be21355e1f4c7edcb4c8a9e3e387cdee623e448
Daven <davensteen@gmail.com>
2022-09-05 20:03:39 -0500

    add pseudocode

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+//create global variables
+
+
+//start quiz function
+
+
+//quiz timer function
+
+
+//highscore funtion
+
+
+//quiz screen function
+
+
+//subnit user info
+
+
+//final page
+
+
+//add event listeners
