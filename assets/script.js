dda12ed1c99b96903b04bbe5729e96cb147569ee
Daven <davensteen@gmail.com>
2022-09-05 22:22:25 -0500

    added start function to script.js

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,29 @@
 //create global variables
-
+var question = document.querySelector(".question");
+var li1 = document.querySelector("#option-1");
+var li2 = document.querySelector("#option-2");
+var li3 = document.querySelector("#option-3");
+var li4 = document.querySelector("#option-4");
+var timeLeft = document.querySelector("time-left");
+let score = timeLeft
+var startQuiz = document.querySelector("#start-btn")
 
 //start quiz function
+function questionPromt() {
+    question.textContent= "hello World"
+    console.log(question)
+}
+
+
+
+
+
 
 
 //quiz timer function
+var startTimer = function() {
+
+}
 
 
 //highscore funtion
@@ -20,3 +39,4 @@
 
 
 //add event listeners
+startQuiz.addEventListener("click", questionPromt)
